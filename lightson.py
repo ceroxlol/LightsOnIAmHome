43b24d534c813afed05ac762ce29ad45203d32ea
Christopher Werner <Christopher.werner1@gmx.net>
2020-03-04 21:46:19 +0100

    Final working version.

--- lightson.py
+++ lightson.py
@@ -6,6 +6,12 @@ from datetime import datetime, timedelta
 
 bridge_ip = None
 device_address = None
+b = None
+
+def calculateTimeDelta(hour, minute):
+    now = datetime.now()
+    tomorrow = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
+    return (timedelta(hours=24) - (now - tomorrow)).total_seconds() % (24 * 3600)
 
 if __name__ == '__main__':
     done_today = False
@@ -13,13 +19,13 @@ if __name__ == '__main__':
     hour, minute = sys.argv[2].split(":")
     device_address = sys.argv[3]
     bridge_ip = sys.argv[4]
+	
+    hour = int(hour)
+    minute = int(minute)
 
     while True:
-        time.sleep(polling_time)
-        p = subprocess.Popen("sudo arp-scan -l -r 3 | grep " + device_address, stdout=subprocess.PIPE, shell=True)
-        (output, err) = p.communicate()
-        p_status = p.wait()
-        if output and not done_today:
+        res = subprocess.call(['ping', '-c', '3', device_address])
+        if res == 0 and not done_today:
             print("Device is connected to the network.")
             if b is None:
                 b = Bridge(bridge_ip)
@@ -27,15 +33,10 @@ if __name__ == '__main__':
                 b.connect()
             b.set_light(['DEVICE1', 'DEVICE2'] , 'on', True)
             done_today = True
-            output = None
             
         elif done_today:
-            print("Sleeping until tomorrow...")
             done_today= False
-            time.sleep(calculatteTimeDelta(hour, minute))
-            
-            
-def calculatteTimeDelta(hour, minute):
-    now = datetime.now()
-    tomorrow = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
-    return (timedelta(hours=24) - (now - tomorrow)).total_seconds() % (24 * 3600)
+            print("Sleeping until tomorrow... ("+str(calculateTimeDelta(hour, minute))+" seconds)")
+            time.sleep(calculateTimeDelta(hour, minute))
+        print("sleeping for " + str(polling_time) + " seconds...")
+        time.sleep(polling_time)
